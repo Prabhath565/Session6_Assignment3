b05f47310d04e467a61c7a7dacedeaae9250dda7
Prabhath565 <prabhath565@gmail.com>
2016-12-19 12:45:29 +0530

    S6_3

---
+++ external.js
@@ -0,0 +1,13 @@
+		var sum = 0;
+				for (var i = 1; i <= 1000; i++) {             // for loop condition, updater
+				   sum += i;                                 // body
+				}
+				console.log("Sum = " + sum);
+		
+		
+				 while (i <= 1000) {            // while loop condition
+				 sum += i;                     // body
+				 i++;                         // updater
+				}
+				console.log("Sum = " + sum);     
+				
